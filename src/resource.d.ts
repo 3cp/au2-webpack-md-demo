1a9e3d3973e746f78164f7f776f6043f0b5689f3
Chunpeng Huo <huocp@me.com>
2020-03-27 07:55:39 +1100

    add md file ts def

--- src/resource.d.ts
+++ src/resource.d.ts
@@ -10,5 +10,16 @@ declare module '*.html' {
   export const shadowOptions: { mode: 'open' | 'closed'} | undefined;
   export function register(container: IContainer);
 }
-
+declare module '*.md' {
+  import { IContainer } from '@aurelia/kernel';
+  import { IBindableDescription } from '@aurelia/runtime';
+  export const name: string;
+  export const template: string;
+  export default template;
+  export const dependencies: string[];
+  export const containerless: boolean | undefined;
+  export const bindables: Record<string, IBindableDescription>;
+  export const shadowOptions: { mode: 'open' | 'closed'} | undefined;
+  export function register(container: IContainer);
+}
 declare module '*.css'
